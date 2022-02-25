88a6cad7a520678928b65aa31e8ace91a3fd3147
Ankur Datta <ankur@algosolver.com>
2022-02-25 12:13:02 +0600

    feat: implement cors and update validation pipes

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,13 @@
-import { ValidationPipe } from '@nestjs/common';
+import {
+  UnprocessableEntityException,
+  ValidationError,
+  ValidationPipe,
+} from '@nestjs/common';
 import { ConfigService } from '@nestjs/config';
 import { NestFactory } from '@nestjs/core';
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import { AppModule } from './app.module';
+import * as cors from 'cors';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
@@ -17,10 +22,32 @@ async function bootstrap() {
     .addTag('fish')
     .build();
 
-  app.useGlobalPipes(new ValidationPipe());
+  app.useGlobalPipes(
+    new ValidationPipe({
+      transform: true,
+      transformOptions: {
+        enableImplicitConversion: true,
+      },
+      exceptionFactory: (errors: ValidationError[]) => {
+        const messages = {};
+
+        errors.forEach((obj) => {
+          messages[obj.property] =
+            obj.constraints[Object.keys(obj.constraints)[0]];
+        });
+
+        throw new UnprocessableEntityException({
+          messages,
+          statusCode: 422,
+        });
+      },
+    }),
+  );
 
   const document = SwaggerModule.createDocument(app, config);
   SwaggerModule.setup('docs', app, document);
+
+  app.use(cors());
 
   await app
     .listen(port)
